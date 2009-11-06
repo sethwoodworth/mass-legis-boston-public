872fe69bae26e9bf14743dc4d8b6ceec7562b393
Seth Woodworth <seth@isforTea.(none)>
2009-11-06 15:33:03 -0500

    initial commit add url ranges to crawll

---
+++ crawling_notes.sh
@@ -0,0 +1,54 @@
+
+	http://www.mass.gov/legis/'{184-186}'history/
+	http://www.mass.gov/legis/bills/senate/
+	http://www.mass.gov/legis/bills/house/
+	#Commitee assignments
+	http://www.mass.gov/legis/commenu.htm
+	#Laws of the 01 (##) session
+	http://www.mass.gov/legis/laws/seslaw01/
+	#Mass General Laws
+	http://www.mass.gov/legis/laws/mgl/
+	#Desc of the representative districts of MASS
+	http://www.mass.gov/legis/repdis03.htm
+	http://www.mass.gov/legis/repdis01.htm
+	#Journal of Senate activity
+	http://www.mass.gov/legis/journal/sj031298.htm
+	http://www.mass.gov/legis/journal/sj%MM%DD%YY.htm
+	#Journal of House activity (PDF)
+	http://www.mass.gov/legis/journal/main.htm
+	http://www.mass.gov/legis/journal/journals'{01-08}'.htm
+	#General reports (directory not available, link crawl)
+	http://www.mass.gov/legis/reports/repindex.htm
+	#98 & 99 budgets
+	http://www.mass.gov/legis/senate/99budget/contents.htm
+	http://www.mass.gov/legis/senate/98budget/contents.htm
+	#FY10 budget
+	http://www.mass.gov/legis/10budget/house/html/waysmeans.htm
+
+	## Unsure/unsorted
+	#Budget spreadsheet reports?
+	http://www.mass.gov/legis/reports/table'{4-11}'.htm
+	http://www.mass.gov/legis/09budget/house/book/FY09Sponsor.pdf
+	http://www.mass.gov/legis/10budget/senate/fy10_localaid.xls
+
+	## Covered elsewhere
+	# Report(s)
+	http://www.mass.gov/legis/bills/senate/186/st02/st02062.htm
+
+
+	#Need to crawl daily
+	#Calenders
+	http://www.mass.gov/legis/senate/calendar.htm
+	http://www.mass.gov/legis/senate/jtcalendar.htm
+	## PDF's
+	#Calender
+	http://www.mass.gov/legis/house/calendar.pdf
+	#Most Recent Journal
+	http://www.mass.gov/legis/house/journal.pdf
+
+
+	## Scrape/Parse notes
+	http://www.mass.gov/legis/10budget/house/amendments/1-500/FY10-0001.htm
+	"Mr. Puppolo of Springfield"
+	"Springfield Business Improvement District (SBID)"
+	"City of Springfield"
